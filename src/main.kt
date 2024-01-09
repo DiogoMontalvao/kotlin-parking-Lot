724e0c099f1c82d356b379392c28c8c19e5468ea
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2024-01-09 19:43:00 -0300

    While loops repetition

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,15 @@
-fun main() {
+import java.util.*
 
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    val lista = mutableListOf<Int>()
+    var value: Int
+
+    do {
+            value = scanner.nextInt()
+            lista.add(value)
+    } while ((value != 0))
+
+    println(lista.max())
 }
