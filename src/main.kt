c4c2ed9ea0047a9649ebeff7e590eaad4d6fffb2
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2024-01-18 06:33:29 -0300

    Update

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,7 @@
 import java.util.*
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-
-    val lista = mutableListOf<Int>()
-    var value: Int
-
-    do {
-            value = scanner.nextInt()
-            lista.add(value)
-    } while ((value != 0))
-
-    println(lista.max())
+    println(('A'..'Z').toList())
+    println()
+    println(('A').rangeTo('Z').toList())
 }
