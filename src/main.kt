c34f738e1eddec01f32e850cee42c274282dddfc
Diogo Montalvão <109600744+DiogoMontalvao@users.noreply.github.com>
2024-02-16 19:04:14 -0300

    Compression algorithm

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,25 @@
-import java.util.*
-
 fun main() {
-    println(('A'..'Z').toList())
-    println()
-    println(('A').rangeTo('Z').toList())
+    val word = readln()
+
+    var charCounter = 0
+    var nextChar = 1
+
+    if (word.isEmpty()) {
+        return
+    } else {
+        for (currentChar in word) {
+            charCounter++
+
+            if (nextChar == word.length) {
+                print("$currentChar$charCounter")
+            } else if (currentChar == word[nextChar]) {
+                nextChar++
+            } else {
+                print("$currentChar$charCounter")
+                charCounter = 0
+                nextChar++
+            }
+        }
+    }
 }
+
