389eec87f50a0dde43f10ef17e39a1d8de6bb376
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2024-09-05 19:24:25 -0300

    Melhorinha

--- src/main.kt
+++ src/main.kt
@@ -3,20 +3,17 @@ import java.util.*
 val scanner = Scanner(System.`in`)
 
 fun main() {
-    val batata = MutableList(5) { (1..100).random() }
+    val batata = MutableList(10) { (1..100).random() }
     println(batata)
 
-    println(ordenacaoSelecao(batata))
-    println(ordenacaoSelecaoReversa(batata))
+    println(batata.ordenacaoSelecaoReversa())
 }
 
-fun ordenacaoSelecaoReversa(_lista: MutableList<Int>): List<Int> {
-    val lista = _lista.toMutableList()
+fun MutableList<Int>.ordenacaoSelecaoReversa(): List<Int> {
+    val lista = this.toMutableList()
     val novaLista = MutableList<Int>(0) { Int.MIN_VALUE }
 
-    for (i in 0..lista.size) {
-        if (lista.size == 0) break
-
+    for (i in 0..lista.lastIndex) {
         val iMaiorN = encontraIndiceMaior(lista)
         novaLista.add(lista.removeAt(iMaiorN))
     }
@@ -30,61 +27,10 @@ fun encontraIndiceMaior(lista: MutableList<Int>): Int {
 
     lista.forEachIndexed { index, numero ->
         if (numero > maiorN) {
-            maiorN = lista[index]
+            maiorN = numero
             indiceMaiorN = index
         }
     }
 
     return indiceMaiorN
-}
-
-fun ordenacaoSelecao(_lista: MutableList<Int>): List<Int> {
-    val lista = _lista.toMutableList()
-    val novaLista = MutableList<Int>(0) { Int.MAX_VALUE }
-
-    for (i in 0..lista.size) {
-        if (lista.size == 0) break
-
-        val iMenorN = encontraIndiceMenor(lista)
-        novaLista.add(lista.removeAt(iMenorN))
-    }
-
-    return novaLista
-}
-
-fun encontraIndiceMenor(lista: MutableList<Int>): Int {
-    var menorN = lista[0]
-    var indiceMenorN = 0
-
-    lista.forEachIndexed { index, numero ->
-        if (numero < menorN) {
-            menorN = lista[index]
-            indiceMenorN = index
-        }
-    }
-
-    return indiceMenorN
-}
-
-fun pesquisaBinaria(lista: MutableList<Int>, numero: Int): Int? {
-    var base = 0
-    var topo = lista.lastIndex
-
-    var operacoes = 0
-    while (base <= topo) {
-        operacoes++
-
-        val meio = (topo + base) / 2
-        val chute = lista[meio]
-
-        if (chute == numero) {
-            println(operacoes)
-            return meio
-        }
-        if (chute > numero) topo = meio - 1
-        if (chute < numero) base = meio + 1
-    }
-
-    println(operacoes)
-    return null
 }
